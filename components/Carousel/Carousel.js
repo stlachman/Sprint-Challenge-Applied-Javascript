1ef684ef9be99cb37c6db7f225276fe7e0f50512
Seth Lachman <lachmanseth17@gmail.com>
2019-04-12 13:24:43 -0400

    Added functionality to right button and updated styles for top bar.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,44 +1,43 @@
 class Carousel {
-  constructor(carousel) {
-    this.carousel = carousel;
-    this.leftButton = carousel.querySelector('.left-button');
-    this.rightButton = carousel.querySelector('.right-button');
-    
-    this.images = carousel.querySelectorAll('img');
-    this.currentIndex = 0;
-    
-    
-    
+	constructor(carousel) {
+		this.carousel = carousel;
+		this.leftButton = carousel.querySelector('.left-button');
+		this.rightButton = carousel.querySelector('.right-button');
 
-    this.leftButton.addEventListener('click', () => {
-      this.slideLeft();
-    });
+		this.images = carousel.querySelectorAll('img');
+		this.currentIndex = 0;
 
-    this.rightButton.addEventListener('click', () => {
-      this.slideRight();
-    });
-  }
-  slideLeft() {
-    if (this.currentIndex > 0) {
-      this.images.forEach(image => image.style.display = "none");
-      this.currentIndex--;
-      this.images[this.currentIndex].style.display = 'block';
-    }
-  }
+		this.leftButton.addEventListener('click', () => {
+			this.slideLeft();
+		});
 
-  slideRight() {
-    if (this.currentIndex < this.images.length - 1) {
-      this.images.forEach(image => image.style.display = "none");
-      this.currentIndex++;
-      this.images[this.currentIndex].style.display = 'block';
-    }
-  }
+		this.rightButton.addEventListener('click', () => {
+			this.slideRight();
+		});
+	}
+	slideLeft() {
+		if (this.currentIndex > 0) {
+			this.images.forEach((image) => (image.style.display = 'none'));
+			this.currentIndex--;
+			this.images[this.currentIndex].style.display = 'block';
+		} 
+	}
+
+	slideRight() {
+		if (this.currentIndex < this.images.length - 1) {
+			this.images.forEach((image) => (image.style.display = 'none'));
+			this.currentIndex++;
+			this.images[this.currentIndex].style.display = 'block';
+		} else {
+			this.images.forEach((image) => (image.style.display = 'none'));
+			this.currentIndex = 0;
+			this.images[this.currentIndex].style.display = 'block';
+		}
+	}
 }
-
 
 let carousel = document.querySelector('.carousel');
 const fancyCarousel = new Carousel(carousel);
-
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
